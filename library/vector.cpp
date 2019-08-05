7f6e8cf6d0c3ae494db3c32685026d0fe6cb9512
namhoon <namhong2001@gmail.com>
2019-08-05 14:21:31 +0900

    1

--- library/vector.cpp
+++ library/vector.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 const double PI = acos(-1);
 
-
-
+struct vector2 {
+	double x, y;
+	explicit vector2(double x=0, double y=0): x(x), y(y) {};
+	bool operator == (const vector2& rhs) const {
+		return x == rhs.x && y == rhs.y;
+	}
+	bool operator < (const vector2& rhs) const { 
+		return x != rhs.x ? x < rhs.x : y < rhs.y;
+	}
+	vector2 operator + (const vector2& rhs) const {
+		return vector2(x+rhs.x, y+rhs.y);
+	}
+	vector2 operator - (const vector2& rhs) const {
+		return vector2(x-rhs.x, y-rhs.y);
+	}
+	vector2 operator * (double rhs) const {
+		return vector2(x*rhs, y*rhs);
+	}
+	double magnitude() const {
+		return hypot(x, y);
+	}
+	vector2 unit() const {
+		return vector2(x / magnitude(), y / magnitude());
+	}
+	double polar() const {
+		return fmod(atan2(y, x) + 2*PI, 2*PI);
+	}
+	double dot(const vector2& rhs) const {
+		return x*rhs.x + y*rhs.y;
+	}
+	double cross(const vector2& rhs) const {
+		return x*rhs.y - y*rhs.x;
+	}
+	vector2 project(const vector2& rhs) const {
+		vector2 r = rhs.unit();
+		return r*r.dot(*this);
+	}
+};
 
 int main() { 
-
 
 	return 0;
 }
