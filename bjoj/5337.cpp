d7835b1bfde2658888540ce715c457556060518f
namhoon <namhong2001@gmail.com>
2019-12-22 12:04:39 +0900

    add vscode settings

---
+++ bjoj/5337.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+const string ans = "\
+.  .   .\n\
+|  | _ | _. _ ._ _  _\n\
+|/\\|(/.|(_.(_)[ | )(/.\n\
+";
+
+int main() {
+    cout << ans;
+    return 0;
+}
---
+++ bjoj/5338.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const string aa = "\
+       _.-;;-._\n\
+'-..-'|   ||   |\n\
+'-..-'|_.-;;-._|\n\
+'-..-'|   ||   |\n\
+'-..-'|_.-''-._|\n\
+";
+    cout << aa;
+    return 0;
+}
