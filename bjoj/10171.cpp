b639b4fa08f6dc14325e55fac51de70f33787e02
namhoon <namhong2001@gmail.com>
2019-12-22 12:54:06 +0900

    1

---
+++ bjoj/10171.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+const string ans = "\
+\\    /\\\n\
+ )  ( ')\n\
+(  /  )\n\
+ \\(__)|\n\
+";
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  cout << ans;
+
+  return 0;
+}
