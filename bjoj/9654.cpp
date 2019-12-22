0796cd543d525459f17057eee85109cc0dd0b60e
namhoon <namhong2001@gmail.com>
2019-12-22 12:47:34 +0900

    set task to point absolute path

--- bjoj/9654.cpp
+++ bjoj/9654.cpp
@@ -6,7 +6,7 @@ const string ans = "\
 SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n\
 N2 Bomber      Heavy Fighter  Limited    21        \n\
 J-Type 327     Light Combat   Unlimited  1         \n\
-NX Cruiser     Medium Fighter Limited    18        \n\
+NX Cruiser     MediuBm Fighter Limited    18        \n\
 N1 Starfighter Medium Fighter Unlimited  25        \n\
 Royal Cruiser  Light Combat   Limited    4         \n\
 ";
